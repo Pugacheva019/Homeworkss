e66001f5e8529562ff7785c30911390aff8c93da
Pugacheva019 <anita20012012@icloud.com>
2020-03-02 00:41:09 +0300

    task 2.2

---
+++ hashtable/IHashFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hashtable
+{
+    /// <summary>
+    /// Class for hash function.
+    /// </summary>
+    public class IHashFunction
+    {
+        public int HashFunction(int key, int size)
+        {
+            return key % size;
+        }
+    }
+}
