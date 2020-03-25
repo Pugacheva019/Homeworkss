afc824559b0110bc6dc21199f6aa2a7504f68ef7
Pugacheva019 <anita20012012@icloud.com>
2020-03-25 16:47:29 +0300

    task 1.2 with correct (in new repository)

---
+++ fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+	public class Program
+	{
+		public static int Fibonacci(int number)
+		{
+			if (number == 1 || number == 2)
+			{
+				return number -= 1;
+			}
+			return Fibonacci(number - 1) + Fibonacci(number - 2);
+		}
+
+		public static void Main()
+		{
+			Console.WriteLine("Enter the number of the Fibonacci number");
+			var number = int.Parse(Console.ReadLine());
+			Console.WriteLine($"Fibonacci number = {Fibonacci(number)}");
+		}
+	}
+}
