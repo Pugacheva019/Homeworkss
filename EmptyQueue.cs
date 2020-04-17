d636a73c94985041d3484dc2d62c0ecffc017a06
Pugacheva019 <anita20012012@icloud.com>
2020-04-17 17:21:08 +0300

    test 1

---
+++ EmptyQueue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Test1
+{
+    /// <summary>
+    /// Exception for dequeuing an element from empty queue
+    /// </summary>
+    public class EmptyQueue : Exception
+    {
+        public EmptyQueue() { }
+        public EmptyQueue(string message) : base(message) { }
+        public EmptyQueue(string message, Exception inner) : base(message, inner) { }
+        protected EmptyQueue(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test1
+{
+    /// <summary>
+    /// Priority queue's class
+    /// </summary>
+    public class Queue<T, TItem>
+    {
+        readonly SortedDictionary<T, Queue<TItem>> Queues;
+
+        public Queue(IComparer<T> priorityComparer)
+        {
+            Queues = new SortedDictionary<T, Queue<TItem>>(priorityComparer);
+        }
+
+        public Queue() : this(Comparer<T>.Default) { }
+
+        public void Enqueue(T priority, TItem item)
+        {
+            if (!Queues.ContainsKey(priority))
+            {
+                Add(priority);
+            }
+
+            Queues[priority].Enqueue(item);
+        }
+
+        private void Add(T priority)
+        {
+            Queues.Add(priority, new Queue<TItem>());
+        }
+
+        public TItem Dequeue()
+        {
+            if (Queues.Any())
+                return Dequeue2();
+            else
+                throw new EmptyQueue("The queue is empty");
+        }
+
+        private TItem Dequeue2()
+        {
+            KeyValuePair<T, Queue<TItem>> one = Queues.First();
+            TItem Item = one.Value.Dequeue();
+            if (!one.Value.Any())
+            {
+                Queues.Remove(one.Key);
+            }
+            return Item;
+        }
+    }
+}
